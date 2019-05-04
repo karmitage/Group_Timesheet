8005a978a21ab56413485a76468746a9e81b5929
katherine <kmarmitage@gmail.com>
2019-05-04 15:36:46 -0700

    got date stuff working, added calculated display values

--- assets/app.js
+++ assets/app.js
@@ -15,32 +15,38 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // Capture user inputs and store them into variables
+// add a date format variable that handles start date inputs
 var name = '';
 var startDate = '';
 var hourlyRate = 0;
 var role = '';
+var dateFormat = 'MM-DD-YYYY'
 
 
 $("#search").on("click", function (event) {
     // prevent page from refreshing when form tries to submit itself
     event.preventDefault();
 
-    // Capture user inputs and store them into variables
+    // Capture user inputs and store them in variables
     name = $("#name").val().trim();
     startDate = $("#date").val().trim();
     hourlyRate = $("#rate").val().trim();
     role = $("#role").val().trim();
 
-    var randomDate =
+    //this captures the "start date" field input (which is a string), stores it in 
+    //the format specified by the "dateFormat" variable and displays it in that format
+    //if we wanted, we could store it in one format and display it in another. but we're not going to do that.
 
-        // Console log each of the user inputs to confirm we are receiving them
-        console.log(name);
+    startDate = moment(startDate, dateFormat).format(dateFormat);
+
+    // Console log each of the user inputs to confirm we are receiving them
+    console.log(name);
     console.log(startDate);
     console.log(hourlyRate);
     console.log(role);
 
 
-    // Code for handling the push
+    // push data to our db
     database.ref().push({
         name: name,
         role: role,
@@ -56,44 +62,32 @@ database.ref().on("child_added", function (snapshot) {
     // storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
 
-    // Console.loging the last user's data
+    // Console.logging data
     console.log(sv.name);
     console.log(sv.role);
     console.log(sv.startDate);
     console.log(sv.hourlyRate);
 
-    //Create html elements --could this be a function?
+    //initialize variables for calculated display values
+    var months = '';
+    var total = '';
 
-    // var row = $("<div class='row'>");
+    //months worked is the difference in months between start date and current date
+    //this diff function takes the current date (moment()) and calculates the diff
+    //btw it and the startDate, formatted using our date format
 
-    // var col1 = $("<div class='col' id='name-display'>");
-    // var col2 = $("<div class='col' id='role-display'>");
-    // var col3 = $("<div class='col' id='date-display'>");
-    // var col4 = $("<div class='col' id='rate-display'>");
-    // var col5 = $("<div class='col' id='months-display'>");
-    // var col6 = $("<div class='col' id='total-display'>");
+    months = moment().diff(moment(sv.startDate, dateFormat), "months");
+    console.log("number of months: " + months);
 
-    // var months = '';
-    // var total = '';
+    //create a variable to hold the weekly rate
+    var weeklyRate = parseInt(sv.hourlyRate) * 40;
+    console.log("weekly rate is: " + weeklyRate);
 
-    // col1.text(sv.name);
-    // col2.text(sv.role);
-    // col3.text(sv.startDate);
-    // col4.text(sv.hourlyRate);
-    // col5.text(months);
-    // col6.text(total);
+    //assign total amount billed to the rate * the number of weeks since hire date
+    total = weeklyRate * moment().diff(moment(sv.startDate, dateFormat), "weeks");
+    console.log("total billed: " + total);
 
-    // row.append(col1);
-    // row.append(col2);
-    // row.append(col3);
-    // row.append(col4);
-    // row.append(col5);
-    // row.append(col6);
-
-    // $("#displayResults").append(row);
-
-    //new logic that references actual HTML elements
-
+    //populate actual HTML elements
     var row = $("<tr>");
     var rowHeader = $("<th scope='row'>");
     row.append(rowHeader);
@@ -106,9 +100,6 @@ database.ref().on("child_added", function (snapshot) {
     var col4 = $("<td id='months-display'>");
     var col5 = $("<td id='total-display'>");
 
-
-    var months = '';
-    var total = '';
 
     rowHeader.text(sv.name);
     col1.text(sv.role);
